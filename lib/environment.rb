d66e6aaf886ed6ace4a23a0d1c95fafe0d14ae44
Gary Wheeler <gkwheeler40@gmail.com>
2020-01-21 02:50:03 +0000

    added pry

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
 require_relative "./Techmeme_Reader/version"
 require_relative "./Techmeme_Reader/CLI"
 
+require 'pry'
+
 module TechmemeReader
   class Error < StandardError; end
   # Your code goes here...
